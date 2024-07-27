ec8501493f9caecc4ec977d30c144014748bcc32
vianney.sonneville <vianney.sonneville@unova.fr>
2024-07-27 03:36:13 +0200

    #

--- lib/queulor_views/cpns/dropdown.rb
+++ lib/queulor_views/cpns/dropdown.rb
@@ -5,18 +5,19 @@ require "rails"
 
 module QueulorViews::Cpns
   class Dropdown < ViewComponent::Base
-    attr_reader :button_name, :items
+    attr_reader :button_name, :items, :opts
 
-    def initialize(button_name: :nil, items: [])
+    def initialize(button_name: :nil, items: [], **opts)
       super
       @button_name = button_name
       @items = items
+      @opts = opts
     end
 
     erb_template <<-ERB
       <div class="queulor_dropdown" data-controller="queulor-views--queulor-dropdown">
         <button class="queulor_dropdown__button" data-action="click->queulor-views--queulor-dropdown#toggle"><%= button_name %></button>
-        <div class="queulor_dropdown__content" data-queulor-views--queulor-dropdown-target="content" data-queulor-views--queulor-dropdown-show-class="show">
+        <div class="queulor_dropdown__content" data-queulor-views--queulor-dropdown-target="content" data-queulor-views--queulor-dropdown-show-class="<%= opt[:data-class] ?? 'show' %>">
           <% items.each do |item| %>
             <%= line_for item %>
           <% end %>
