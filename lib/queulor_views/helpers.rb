fcbbb9c9e0ba5f58bd41e2fe9e2c205629e87641
vianney.sonneville <vianney.sonneville@unova.fr>
2024-07-27 05:50:11 +0200

    #

--- lib/queulor_views/helpers.rb
+++ lib/queulor_views/helpers.rb
@@ -5,7 +5,7 @@ require "queulor_views/cpns/dropdown"
 module QueulorViews
   module Helpers
     def dropdown(button_name:, items:, **opts)
-      QueulorViews::Cpns::Dropdown.new button_name:, items:, opts
+      QueulorViews::Cpns::Dropdown.new button_name:, items:
     end
   end
 end
