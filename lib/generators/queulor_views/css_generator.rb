ef2c1786d66a20f6f0df8877f5b3f1b56785511a
vianney.sonneville <vianney.sonneville@unova.fr>
2024-07-27 04:02:25 +0200

    #

---
+++ lib/generators/queulor_views/css_generator.rb
@@ -0,0 +1,15 @@
+require 'rails/generators'
+
+module QueulorViews
+  class JsGenerator < Rails::Generators::Base
+    def install
+      Dir.glob("#{__dir__}/assets/stylesheets/*.css") do |file|
+        copy_file file, "app/assets/stylesheets/queulor_views/#{file.split("/").last}"
+      end
+    end
+
+    def self.source_root
+      File.join(File.dirname(__FILE__), '..', '..', 'assets', 'stylesheets')
+    end
+  end
+end
--- lib/generators/queulor_views/js_generator.rb
+++ lib/generators/queulor_views/js_generator.rb
@@ -1,15 +1,15 @@
 require 'rails/generators'
 
 module QueulorViews
-  class InstallGenerator < Rails::Generators::Base
+  class JsGenerator < Rails::Generators::Base
     def install
       Dir.glob("#{__dir__}/assets/javascripts/*.js") do |file|
         copy_file file, "app/javascript/controllers/queulor_views/#{file.split("/").last}"
       end
+    end
 
-      Dir.glob("#{__dir__}/assets/stylesheets/*.css") do |file|
-        copy_file file, "app/assets/stylesheets/queulor_views/#{file.split("/").last}"
-      end
+    def self.source_root
+      File.join(File.dirname(__FILE__), '..', '..', 'assets', 'javascripts')
     end
   end
 end
