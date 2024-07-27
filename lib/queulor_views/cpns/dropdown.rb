d1eb4dd3118292a44e7e9f37b4070e9a1325556e
vianney.sonneville <vianney.sonneville@unova.fr>
2024-07-27 03:22:45 +0200

    js

--- lib/queulor_views/cpns/dropdown.rb
+++ lib/queulor_views/cpns/dropdown.rb
@@ -15,10 +15,12 @@ module QueulorViews::Cpns
 
     erb_template <<-ERB
       <div class="queulor_dropdown" data-controller="queulor-views--queulor-dropdown">
-        <button class="queulor_dropdown__button"><%= button_name %></button>
-        <% items.each do |item| %>
-          <%= line_for item %>
-        <% end %>
+        <button class="queulor_dropdown__button" data-action="click->queulor-views--queulor-dropdown#toggle"><%= button_name %></button>
+        <div class="queulor_dropdown__content" data-queulor-dropdown-target="content">
+          <% items.each do |item| %>
+            <%= line_for item %>
+          <% end %>
+        </div>
       </div>
     ERB
 
