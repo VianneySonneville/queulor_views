06f424c883e33e710574367512967e6cdbc44abc
vianney.sonneville <vianney.sonneville@unova.fr>
2024-07-29 20:23:22 +0200

    helpers

--- lib/queulor_views/helpers.rb
+++ lib/queulor_views/helpers.rb
@@ -4,6 +4,19 @@ require "queulor_views/cpns/dropdown"
 
 module QueulorViews
   module Helpers
+    def accordion(sections: [], html_attributes: {})
+      QueulorViews::Cpns::Dropdown.new(html_attributes:) do |cpn|
+        sections.each do |section|
+          cpn.with_section(
+            title: section[:title],
+            summary: section[:summary],
+            expanded: section[:expanded],
+            html_attributes: section[:html_attributes]
+          )
+        end
+      end
+    end
+
     def dropdown(button_name:, items:, **opts)
       QueulorViews::Cpns::Dropdown.new button_name:, items:, opts:
     end
